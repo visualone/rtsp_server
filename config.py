a9cb4e500dec8ad327b1db05f03673caaea4f22b
Mohammad Rafiee <rafiee.cal@gmail.com>
2021-03-03 19:53:13 -0800

    clean up

--- config.py
+++ config.py
@@ -2,9 +2,6 @@
 # The port to be used by the Flask server
 FLASK_SERVER_PORT = 8181
 
-# A list of IP addresses that are allowed to access the endpoint.
-ALLOWED_REMOTE_IP_ADDRESSES = ['127.0.0.1', '35.155.73.2']
-
 # A dict of camera ids and their RTSP urls.
 cameras = {
     "WP6XcgtOr2": "rtsp://multipolar:t3rp999@192.168.0.106/live"
--- rtsp_server.py
+++ rtsp_server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, abort
+from flask import Flask, request
 import os
 import cv2
 from PIL import Image
@@ -25,60 +25,22 @@ for camera_id in config.cameras:
                          stdout=subprocess.PIPE, 
                          stderr=subprocess.STDOUT)
 
-@app.route('/frame2')
-def frame2():
-  camera_id = request.args.get('camera_id')
-  frame_file = os.path.join(app.config['FRAMES_FOLDER'], '{}.jpg'.format(camera_id))
-  return render_template("index.html", current_frame = frame_file + "?" + str(time.time()) )
-
-@app.route('/get_image')
-def get_image():
-    camera_id = request.args.get('camera_id')
-    frame_file = os.path.join(app.config['FRAMES_FOLDER'], '{}.jpg'.format(camera_id))
-    return send_file(frame_file, mimetype='image/jpg')
-
+    
 @app.route("/test")
 def test():
     return "The RTSP server is running!\n"
 
 
-@app.route('/frame')
-def frame():
+@app.route('/get_image')
+def get_image():
+    camera_id = request.args.get('camera_id')
     
-    if not request.remote_addr in config.ALLOWED_REMOTE_IP_ADDRESSES:
-        abort(403)
-    
-    local_camera_id = request.args.get('local_camera_id')
-    
-    if not local_camera_id in config.cameras:
+    if not camera_id in config.cameras:
         return "Invalid camera id."
     
-    vc = vcs[local_camera_id]
+    frame_file = os.path.join(app.config['FRAMES_FOLDER'], '{}.jpg'.format(camera_id))
     
-    frame_file = os.path.join(app.config['FRAMES_FOLDER'], '{}.jpg'.format(local_camera_id))
-    
-    if not vc.isOpened():
-        return "Invalid RTSP URL. Please, try again."
-    
-    vc.set(cv2.CAP_PROP_POS_FRAMES, -1)
-    
-    ret, frame = vc.read()
-
-    if ret:
-        frame = np.flip(frame, 2)
-        
-        h, w = frame.shape[:2]
-        
-        image = Image.fromarray(frame)
-        
-        if 'width' in request.args:
-            width = int(request.args.get('width'))
-            image = image.resize((width, round(width / w * h)))
-            
-        image.save(frame_file)
-        return render_template("index.html", current_frame = frame_file + "?" + str(time.time()) )
-    else:
-        return "An error occurred. Could not get the frame."
+    return send_file(frame_file, mimetype='image/jpg')
 
 
 if  __name__ == '__main__':
